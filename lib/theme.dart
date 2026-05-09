f305accfea002af95c578bd9d69ac0ff94402b7b
Talha Khalid <talhakhalidlkp@gmail.com>
2026-05-09 11:36:58 +0500

    Final polish and documentation

--- lib/theme.dart
+++ lib/theme.dart
@@ -25,7 +25,7 @@ class AppTheme {
       elevation: 0,
       centerTitle: true,
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       color: surface,
       elevation: 0,
       shape: RoundedRectangleBorder(
